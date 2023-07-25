ffcd4238fb0f4cfafb2abb34a2ca336bdccf16e9
Katie <clearykatelyn@gmail.com>
2023-07-25 20:15:59 +1000

    drew a crab lol

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
-
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
-
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    println!(r"  |---------------------|");
+    println!(r"  |  WELCOME TO MY APP  |");
+    println!(r"  |---------------------|");
+    println!(r"  |                     |");
+    println!(r" /\/\ (O)       (O)   /\/\ ");
+    println!(r" \  /  \\ ______//    \  / ");
+    println!(r" / /   /         \     \ \ ");
+    println!(r" \ \  |///   V   ///   / / ");
+    println!(r"  \ \ \    ___     \  / /  ");
+    println!(r"   \ \/    ___      \/ /   ");
+    println!(r"    |     _______     |    ");
+    println!(r"    |________________/     ");
 }
